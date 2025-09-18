9bf17af24ac7d7fef282bb1d3b213badd1d76c03
NullStellaris <davidling77@gmail.com>
2025-09-18 17:56:44 +0800

    Added world sprites, implement jumping and sprite direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,23 +13,28 @@ public class PlayerMovement : MonoBehaviour {
     private int fixedUpdateRate;
     // physics variables (in world units, 1 tile = 1 world unit = 16px)
     public float gravity = 50;
-    public float maxSpeed = 5;
-    public float accel = 0.5f;
-    public float airAccel = 0.3f;
-    public float accelSmoothing = 0.8f;
+    public float maxSpeed = 7;
+    public float accel = 0.7f;
+    public float airAccel = 0.4f;
+    public float accelSmoothing = 0.5f;
     public float decel = 0.4f;
     public float jumpAccel = 12;
-    public float varJumpGravScale = 0.4f;
-    public float varJumpDuration = 0.5f;
-    public float varJumpDeadzone = 0.2f;
+    public float varJumpGravScale = 0.12f;
+    public float varJumpDuration = 0.3f;
     // input state variables
     private float directionState = 0; // no need to Sign() this since its already unitized
     private bool jumpState = false;
+    private bool jumpReleaseState = false;
     // game state variables
     private bool onGroundState = true;
     private float varJumpTimer = 0;
+    private bool jumped = false;
     // physics bodies
     private Rigidbody2D marioBody;
+    // sprite variables
+    private SpriteRenderer marioSprite;
+    // sprite state variables
+    private bool faceRightState = true;
 
     // Start is called before the first frame update
     void Start() {
@@ -37,15 +42,31 @@ public class PlayerMovement : MonoBehaviour {
         fixedUpdateRate = (int)(1 / Time.fixedDeltaTime);
         Debug.Log(fixedUpdateRate);
         // Set to be 30 FPS
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
         marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update() {
         // We do input monitoring here since execution is guaranteed every frame
         directionState = Input.GetAxisRaw("Horizontal");
-        jumpState = Input.GetKeyDown("space");
+        if (Input.GetKeyDown("space")) {
+            jumpState = true;
+        }
+        if (Input.GetKeyUp("space")) {
+            jumpReleaseState = true;
+        }
+        // Sprite updates
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState) {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+        if (Input.GetKeyDown("d") && !faceRightState) {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col) {
@@ -71,25 +92,36 @@ public class PlayerMovement : MonoBehaviour {
             // decelerate to stop
             resultVelo.x = Mathf.MoveTowards(resultVelo.x, 0, decel);
         }
-        // apply to mario
-        marioBody.linearVelocity = resultVelo;
 
         // jumping physics
         if (jumpState && onGroundState) {
-            marioBody.AddForce(Vector2.up * jumpAccel, ForceMode2D.Impulse);
+            // start jump
+            resultVelo.y = jumpAccel;
             onGroundState = false;
+            jumped = true;
             varJumpTimer = varJumpDuration;
         }
-        // variable jump physics and gravity
-        float resultGrav = gravity;
-        if (Input.GetKey("space") && !onGroundState && varJumpTimer > 0) {
-            if (varJumpDuration - varJumpTimer < varJumpDeadzone) {
-                Debug.Log("jump held");
-                resultGrav = gravity * varJumpGravScale;
-            }
-            varJumpTimer = Mathf.MoveTowards(varJumpTimer, 0, timeDelta);
-        }
 
-        marioBody.AddForce(Vector2.down * resultGrav);
+        if (jumped) {
+            if (!jumpReleaseState && varJumpTimer > 0) {
+                // less gravity while holding jump key
+                resultVelo.y += -gravity * varJumpGravScale * timeDelta;
+                varJumpTimer -= timeDelta;
+            }
+            else {
+                // back to full grav when key up or if reached max jump
+                resultVelo.y += -gravity * timeDelta;
+                varJumpTimer = 0;
+            }
+        }
+        else {
+            // normal gravity
+            resultVelo.y += -gravity * timeDelta;
+        }
+        marioBody.linearVelocity = resultVelo;
+
+        // clear jumping state flags
+        jumpState = false;
+        jumpReleaseState = false;
     }
 }
